6b950ed36a382d104b162479384de757695dcfda
Aman Ranabhat <080bct013.aman@pcampus.edu.np>
2026-01-12 17:49:15 +0545

    Update details.dart

--- recipe_app/lib/details.dart
+++ recipe_app/lib/details.dart
@@ -14,6 +14,7 @@ class Details extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
+         backgroundColor: Colors.blueGrey,
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
           onPressed: () {
